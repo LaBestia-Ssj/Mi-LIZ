a1222b29caa2515e3ba5ab04ed9fbf8fd3e0014c
LaBestia-Ssj <labestia.ssj3@gmail.com>
2026-03-18 02:02:31 -0600

    Feliz día

---
+++ detalles y sorpresas/Tarjetas/Feliz día/js/main.js
@@ -0,0 +1,7 @@
+setInterval(() => {
+              confetti({
+                  particleCount: 100,
+                  spread: 60,
+                  origin: { x: 0.5, y: 0.5 }
+              });
+          }, 2000); 
